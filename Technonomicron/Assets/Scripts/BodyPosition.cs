21e9fc533694e11b569fb73566a2378a19143524
Tyler S <tyler.g.a.smith@gmail.com>
2018-03-13 15:12:42 -0700

    worked on hooking up avatar to tracking

---
+++ Technonomicron/Assets/Scripts/BodyPosition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyPosition : MonoBehaviour {
+	public Transform target;
+	public Vector3 adjust;
+	public bool fullRotation;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = target.position + adjust;
+		if(!fullRotation)
+		transform.eulerAngles = new Vector3(transform.eulerAngles.x, target.eulerAngles.y, transform.eulerAngles.z);
+		else if (fullRotation) {
+			transform.rotation = target.rotation;
+		}
+	}
+}
--- Technonomicron/Assets/Scripts/Hand.cs
+++ Technonomicron/Assets/Scripts/Hand.cs
@@ -3,16 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Hand : MonoBehaviour {
-	public SteamVR_Controller myController;
+	public SteamVR_TrackedObject myController;
 	public int myIndex;
-
+	public GameObject spellWheel;
 	// Use this for initialization
 	void Start () {
+		//get index of controller this script is on
+		myController = GetComponent<SteamVR_TrackedObject>();
+		myIndex = (int)myController.index;
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		//if the touchpad is touched show the spell wheel
+		if (SteamVR_Controller.Input(myIndex).GetTouch(SteamVR_Controller.ButtonMask.Touchpad)){
+			spellWheel.SetActive(true);
+		}
+		else {
+			spellWheel.SetActive(false);
+		}
 	}
+
+	/*im putting this here because i can never remember it: getting input from steamvr controllers
+	 * 
+	 if (SteamVR_Controller.Input (index).GetPress (SteamVR_Controller.ButtonMask.whateverbutton)) {
+
+	} 
+	 
+	*/
 }
