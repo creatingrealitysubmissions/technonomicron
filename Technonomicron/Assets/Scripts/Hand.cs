acc1c1136be3892d56969578c43eea4735557a79
Tyler S <tyler.g.a.smith@gmail.com>
2018-03-13 12:30:19 -0700

    hand script

---
+++ Technonomicron/Assets/Scripts/Hand.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour {
+	public SteamVR_Controller myController;
+	public int myIndex;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
